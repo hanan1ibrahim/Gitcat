c28519dacfeee0890bd32052463081c05cb29e63
hanan ibrahim <hananibraalmi@gmail.com>
2021-12-10 13:51:21 +0200

    refactor the code of bookmarksVC

--- Gitcat/Controllers/BookmarksView/BookmarksViewController.swift
+++ Gitcat/Controllers/BookmarksView/BookmarksViewController.swift
@@ -7,88 +7,80 @@
 
 import UIKit
 import CoreData
-import SafariServices
 
 class BookmarksViewController: UIViewController {
-    var bookmarkedRepos = [SavedRepositories]()
-    var bookmarkedUsers = [SavedUsers]()
-    // searchLabel appear before searching
-    let conditionLabel: UILabel = {
-        let label = UILabel()
-        label.isHidden = true
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 21, weight: .medium)
-        return label
-    }()
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     //MARK: - @IBOutlets
     @IBOutlet weak var tableView: UITableView!
-    //MARK: - @Props
-    lazy var searchController = UISearchController(searchResultsController: nil)
-    //MARK: - UIcomponent
-    let searchLabel: UILabel = {
-        let label = UILabel()
-        label.isHidden = true
-        label.text = Titles.searchForBookmarks
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 21, weight: .medium)
-        return label
-    }()
+    @IBOutlet weak var searchLabel: UILabel!
+    //MARK: - UI Proprties
+    var searchController = UISearchController(searchResultsController: nil)
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //MARK: - Data Proprties
+    var bookmarkedRepos = [SavedRepositories]()
+    var bookmarkedUsers = [SavedUsers]()
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        initView()
-        renderViewData(tableView: tableView)
+        initUI()
+        initViewData()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        title = Titles.bookmarksViewTitle
-        renderViewData(tableView: tableView)
-        noBookmarksState(tableView: tableView, conditionLabel: conditionLabel)
+        initViewData()
+        initUItitles()
     }
-    override func viewDidLayoutSubviews() {
-        searchLabel.frame = CGRect(x: view.width/4, y: (view.height-200)/2, width: view.width/2, height: 200)
-        conditionLabel.frame = CGRect(x: view.width/4, y: (view.height-200)/2, width: view.width/2, height: 200)
+    //MARK: - UI Functions
+    func initUI() {
+        tableViewData()
+        searchLabelData()
+        initSearchBar()
+        initUItitles()
     }
-    //MARK: - Functions
-    func initView() {
+    func initViewData() {
+        noBookmarksLabelState()
+        fetchCoreDataModels()
+    }
+    func initUItitles() {
         title = Titles.bookmarksViewTitle
         tabBarItem.title = Titles.bookmarksViewTitle
-        conditionLabel.text = Titles.noBookmarks
-        searchController.searchBar.delegate = self
+    }
+    func initSearchBar() {
         setupSearchController(search: searchController)
-        view.addSubview(searchLabel)
-        view.addSubview(conditionLabel)
-        searchLabel.isHidden = true
-        searchLabel.alpha = 0.0
-        tableViewData()
-        noBookmarksState(tableView: tableView, conditionLabel: conditionLabel)
+        searchController.searchBar.delegate = self
+    }
+    func searchLabelData() {
+        searchLabel.text = Titles.noBookmarks
     }
     func tableViewData() {
         tableView.registerCellNib(cellClass: UsersCell.self)
         tableView.registerCellNib(cellClass: ReposCell.self)
         tableView.tableFooterView = UIView()
     }
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        navigationItem.hidesSearchBarWhenScrolling = true
+    func noBookmarksLabelState() {
+        if bookmarkedUsers.isEmpty == true && bookmarkedRepos.isEmpty == true {
+            searchLabel.isHidden = false
+            tableView.isHidden = true
+        } else {
+            searchLabel.isHidden = true
+            tableView.isHidden = false
+        }
     }
-    func renderViewData (tableView: UITableView) {
+    //MARK: - Data Functions
+    func fetchCoreDataModels() {
         DataBaseManger.shared.fetch(returnType: SavedUsers.self) { [weak self] (users) in
             self?.bookmarkedUsers = users
-            tableView.reloadData()
+            self?.tableView.reloadData()
         }
         DataBaseManger.shared.fetch(returnType: SavedRepositories.self) {  [weak self] (reps) in
             self?.bookmarkedRepos = reps
-            tableView.reloadData()
+            self?.tableView.reloadData()
         }
     }
-    func searchFromDB (tableView: UITableView ,searchController: UISearchController) {
+    func searchFromCoreData() {
         guard let searchText = searchController.searchBar.text else { return }
         if searchText.isEmpty {
-            renderViewData(tableView: tableView)
-            DispatchQueue.main.async {
-                tableView.reloadData()
-            }
+            fetchCoreDataModels()
+            tableView.reloadData()
         } else {
             let request : NSFetchRequest<SavedUsers> = SavedUsers.fetchRequest()
             request.predicate = NSPredicate(format: "userName CONTAINS [cd] %@", searchText)
@@ -106,62 +98,21 @@ class BookmarksViewController: UIViewController {
             tableView.reloadData()
         }
     }
-    func noBookmarksState (tableView: UITableView, conditionLabel: UILabel ) {
-        if bookmarkedUsers.isEmpty == true , bookmarkedRepos.isEmpty == true {
-            conditionLabel.isHidden = false
-            tableView.isHidden = true
-        } else {
-            conditionLabel.isHidden = true
-            tableView.isHidden = false
-        }
-    }
-    func deleteAndFetchUsers(at indexPath: IndexPath , tableView: UITableView, conditionLabel: UILabel) {
+    func deleteUsersFromTableView(at indexPath: IndexPath) {
         let item = bookmarkedUsers[indexPath.row]
         DataBaseManger().delete(returnType: SavedUsers.self, delete: item)
         DataBaseManger().fetch(returnType: SavedUsers.self) { (users) in
             self.bookmarkedUsers = users
         }
-        noBookmarksState(tableView: tableView, conditionLabel: conditionLabel)
+        noBookmarksLabelState()
     }
-    func deleteAndFetchRepos(at indexPath: IndexPath , tableView: UITableView, conditionLabel: UILabel) {
+    func deleteRepositoriesFromTableView(at indexPath: IndexPath) {
         let item = bookmarkedRepos[indexPath.row]
         DataBaseManger().delete(returnType: SavedRepositories.self, delete: item)
         DataBaseManger().fetch(returnType: SavedRepositories.self) { (repos) in
             self.bookmarkedRepos = repos
         }
-        noBookmarksState(tableView: tableView, conditionLabel: conditionLabel)
-    }
-}
-//MARK: - SearchBar
-extension BookmarksViewController : UISearchBarDelegate {
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        DispatchQueue.main.async {
-            self.searchController.searchBar.becomeFirstResponder()
-            self.searchLabel.isHidden = false
-        }
-        UIView.animate(withDuration: 0.0, animations: {
-            self.searchLabel.alpha = 1.0
-            self.tableView.alpha = 0.0
-        })
-    }
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        self.searchLabel.isHidden = true
-        searchFromDB(tableView: tableView, searchController: searchController)
-        UIView.animate(withDuration: 0.0, animations: {
-            self.tableView.alpha = 1.0
-            self.searchLabel.alpha = 0.0
-        })
-    }
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        renderViewData(tableView: tableView)
-        DispatchQueue.main.async {
-            self.searchController.searchBar.text = nil
-            self.searchLabel.isHidden = true
-        }
-        UIView.animate(withDuration: 0.0, animations: {
-            self.tableView.alpha = 1.0
-            self.searchLabel.alpha = 0.0
-        })
+        noBookmarksLabelState()
     }
 }
 //MARK:- TableView
@@ -196,21 +147,31 @@ extension BookmarksViewController: UITableViewDelegate , UITableViewDataSource {
         return .delete
     }
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let headerText = UILabel()
         switch section {
         case 0:
-            return Titles.usersViewTitle
+            if bookmarkedUsers.isEmpty == false {
+                headerText.text = Titles.usersViewTitle
+            } else {
+                headerText.text = nil
+            }
         default:
-            return Titles.repositoriesViewTitle
+            if bookmarkedRepos.isEmpty == false {
+                headerText.text = Titles.repositoriesViewTitle
+            } else {
+                headerText.text = nil
+            }
         }
+        return headerText.text
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             tableView.beginUpdates()
             switch indexPath.section {
             case 0:
-                deleteAndFetchUsers(at: indexPath, tableView: self.tableView, conditionLabel: conditionLabel)
+                deleteUsersFromTableView(at: indexPath)
             default:
-                deleteAndFetchRepos(at: indexPath, tableView: self.tableView, conditionLabel: conditionLabel)
+                deleteRepositoriesFromTableView(at: indexPath)
             }
             tableView.deleteRows(at: [indexPath], with: .fade)
             tableView.endUpdates()
@@ -223,5 +184,24 @@ extension BookmarksViewController: UITableViewDelegate , UITableViewDataSource {
         default:
             return 100
         }
+    }
+}
+
+//MARK: - SearchBar
+extension BookmarksViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchLabel.isHidden = false
+        tableView.isHidden = true
+        searchLabel.text = "Search For Bookmarks"
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchLabel.isHidden = true
+        tableView.isHidden = false
+        searchFromCoreData()
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        fetchCoreDataModels()
+        noBookmarksLabelState()
+        searchLabel.text = Titles.noBookmarks
     }
 }
